a9f7c0c26961f55c8fa810ce719d714a20246461
binbinwang <307649087@qq.com>
2021-08-07 14:24:40 +0800

    Initial Commit

---
+++ BNMultiParamDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  BNMultiParamDemo
+//
+//  Created by binbinwang on 2021/8/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ BNMultiParamDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BNMultiParamDemo
+//
+//  Created by binbinwang on 2021/8/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
